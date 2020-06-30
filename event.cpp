17944c6775a329801272969b4625d290307e1105
pcarzon <pcarzon@users.noreply.github.com>
2020-06-30 10:32:09 -0500

    Created Event Structure

--- event.cpp
+++ event.cpp
@@ -1,1 +1,37 @@
 #include "event.h"
+
+Event::Event()
+{
+
+}
+
+Event::~Event()
+{
+
+}
+
+Event::Event(const Event &original)
+{
+
+}
+
+void Event::CopyEvent(const Event &e)
+{
+
+}
+
+Event& Event::operator= (const Event& original)
+{
+	CopyEvent(original);
+	return *this;
+}
+
+vector<vector<double>> Event::SampleEnergy(int location, int radius)
+{
+
+}
+
+void Event::UpdateDensity(int densityType, vector<vector<double>> inputDensity)
+{
+  
+}
--- event.h
+++ event.h
@@ -1,7 +1,34 @@
+#ifndef Event_H
+#define Event_H
+
 #include <iostream>
 #include <string>
 #include <cmath>
 #include <vector>
 
-
 using namespace std;
+
+class Event
+{
+private:
+
+  vector<vector<double>> initial_energy;
+  vector<vector<vector<double>>> density;
+
+  double total_initial_energy;
+  double total_energy;
+  double seed;
+
+	void CopyEvent(const Event &e);
+public:
+  Event();
+  ~Event();
+
+  Event(const Event &original);
+  Event& operator=(const Event& original);
+
+  vector<vector<double>> SampleEnergy(int location, int radius);
+  void UpdateDensity(int densityType, vector<vector<double>> inputDensity);
+
+};
+#endif
