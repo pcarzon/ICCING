81a8fd994bfaad661825fb939776b5433e533992
pcarzon <pcarzon@users.noreply.github.com>
2020-06-30 14:44:24 -0500

    Bare Structure for Eccentricity class

--- ecc.cpp
+++ ecc.cpp
@@ -1,1 +1,27 @@
 #include "ecc.h"
+
+Eccentricity::Eccentricity()
+{
+
+}
+
+Eccentricity::~Eccentricity()
+{
+
+}
+
+Eccentricity::Eccentricity(const Eccentricity &original)
+{
+  CopyEccentricity(original);
+}
+
+void Eccentricity::CopyEccentricity(const Eccentricity &e)
+{
+
+}
+
+Eccentricity& Eccentricity::operator= (const Eccentricity& original)
+{
+	CopyEccentricity(original);
+	return *this;
+}
--- ecc.h
+++ ecc.h
@@ -1,4 +1,7 @@
-#include <iostream>
+#ifndef Eccentricity_H
+#define Eccentricity_H
+
+#include <Eccentricitystream>
 #include <string>
 #include <cmath>
 #include <vector>
@@ -6,3 +9,20 @@
 #include "event.h"
 
 using namespace std;
+
+class Eccentricity
+{
+private:
+
+	void CopyEccentricity(const Eccentricity &e);
+
+public:
+
+  Eccentricity();
+  ~Eccentricity();
+
+  Eccentricity(const Eccentricity &original);
+  Eccentricity& operator=(const Eccentricity& original);
+
+};
+#endif
