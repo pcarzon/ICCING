f830410ca8b2204962b6bf05d57af08da4a52540
pcarzon <pcarzon@users.noreply.github.com>
2020-06-30 14:30:28 -0500

    Update IO class structure

--- io.cpp
+++ io.cpp
@@ -1,27 +1,62 @@
 #include "io.h"
 
-IO::IO()
+IO::IO(string input, string output, int iType, int oType)
 {
-
+  input_file = input;
+  output_file = output;
+  input_type = iType;
+  output_type = oType;
 }
 
 IO::~IO()
 {
-
+  input_file = "";
+  output_file = "";
+  input_type = "";
+  output_type = "";
 }
 
 IO::IO(const IO &original)
 {
-
+  CopyIO(original);
 }
 
 void IO::CopyIO(const IO &e)
 {
-
+  input_file = e.input_file;
+  output_file = e.output_file;
+  input_type = e.input_type;
+  output_type = e.output_type;
 }
 
 IO& IO::operator= (const IO& original)
 {
 	CopyIO(original);
 	return *this;
+}
+
+void IO::OutputFullDensityGrids(const Event &event)
+{
+
+}
+
+void IO::OutputSparseDensityGrids(const Event &event)
+{
+
+}
+
+void IO::OutputEccentricities(const Eccentricity &ecc)
+{
+
+}
+
+Event IO::ReadEvent()
+{
+  Event inputEvent;
+  return inputEvent;
+}
+
+void IO::WriteEvent(const Event &event)
+{
+
 }
--- io.h
+++ io.h
@@ -14,15 +14,26 @@ class IO
 {
 private:
 
+  string input_file;
+  string output_file;
+  int input_type;
+  int output_type;
+
 	void CopyIO(const IO &e);
 
+  void OutputFullDensityGrids(const Event &event);
+  void OutputSparseDensityGrids(const Event &event);
+  void OutputEccentricities(const Eccentricity &ecc);
+
 public:
-  
-  IO();
+
+  IO(string input, string output, int iType, int oType);
   ~IO();
 
   IO(const IO &original);
   IO& operator=(const IO& original);
 
+  Event ReadEvent();
+  void WriteEvent(const Event &event);
 };
 #endif
