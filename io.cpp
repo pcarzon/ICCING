da9cde58e9a98cc39f1673e5ed1b10b85351a44c
pcarzon <pcarzon@users.noreply.github.com>
2020-07-03 10:54:12 -0500

    Testing IO ReadEvent

--- io.cpp
+++ io.cpp
@@ -149,7 +149,7 @@ vector<vector<double>> IO::ReadEvent()
   double value;
 	input.ignore(10000, '\n');
 
-  while (!input.eof() && input.peek() != '\n')
+  while (!input.eof() )
   {
       //cout << "Here it is " << int(input.peek()) << endl;
       input >> value;
@@ -162,6 +162,7 @@ vector<vector<double>> IO::ReadEvent()
       input >> value;
       carrier[x][y] = value;
       cout << value << carrier[x][y] << endl;
+      input.ignore(10000, '\n');
   }
   return carrier;
 }
