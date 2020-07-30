0ac30beba05ca2a229c6ae9acf823a2b0350ae62
pcarzon <pcarzon@users.noreply.github.com>
2020-07-30 11:53:14 -0500

    testing

--- io.cpp
+++ io.cpp
@@ -552,7 +552,7 @@ void IO::OutputSparseDensityGrids(vector<vector<double>> &density_grid, string f
   double x, y, value;
 
 //  for (int i = 0; i < density_grid.size(); i++)
-    for (int i = 0; i <= 55; i++)
+    for (int i = 0; i < 55; i++)
   {
     for (int j = 0; j < density_grid[0].size(); j++)
     {
