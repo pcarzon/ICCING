ef64fc0a852bcbcf41b130e3c30de8a580991fea
pcarzon <pcarzon@users.noreply.github.com>
2020-07-31 15:17:13 -0500

    testing

--- main.cpp
+++ main.cpp
@@ -42,7 +42,7 @@ int main (int argc, char *argv[])
 
 		testSample = testEvent.SampleEnergy();
 
-		testQuarks = machine.SplitSample(testSample)
+		testQuarks = machine.SplitSample(testSample);
 
 		testEvent.UpdateDensity(testQuarks);
 		//	}
