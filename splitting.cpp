01e1bb59575a96b359520f47b1a7313ffb03a683
pcarzon <pcarzon@users.noreply.github.com>
2020-08-03 13:30:27 -0500

    testing code

--- splitting.cpp
+++ splitting.cpp
@@ -57,7 +57,7 @@ double Splitter::RollGlue(double e_tot)
   {
     x = get_energy(get_random_number);
     y = get_probability(get_random_number);
-    cout << x << " " << y << " " << endl;
+  //  cout << x << " " << y << " " << endl;
     num++;
     if (y < 1/pow(x, lambda_)) got_glue = true;
   }
