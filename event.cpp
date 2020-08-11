6f51b2053225922903f6e424e92c02add785c672
pcarzon <pcarzon@users.noreply.github.com>
2020-08-11 13:55:12 -0500

    testing

--- event.cpp
+++ event.cpp
@@ -183,7 +183,7 @@ void Event::UpdateDensity(Quarks quark_density)
         total_initial_energy -= gluon_dist[i][j]*quark_density.GetEnergyFraction()*initial_energy[position[0] - gluon_rad + i][position[1] - gluon_rad + j];
         total_energy += gluon_dist[i][j]*quark_density.GetEnergyFraction()*initial_energy[position[0] - gluon_rad + i][position[1] - gluon_rad + j];
         initial_energy[position[0] - gluon_rad + i][position[1] - gluon_rad + j] -= gluon_dist[i][j]*quark_density.GetEnergyFraction()*initial_energy[position[0] - gluon_rad + i][position[1] - gluon_rad + j];
-        cout << "grid point: " << initial_energy[x_center - gluon_rad + i][y_center - gluon_rad + j] << endl;
+        cout << "grid point: " << initial_energy[position[0] - gluon_rad + i][position[1] - gluon_rad + j] << endl;
       }
     }
     cout << "Total Energy: " << total_initial_energy << " " << total_energy << endl;
