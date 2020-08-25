b2757d57c0a09b9e4fc26b4f114266540474fbe9
pcarzon <pcarzon@users.noreply.github.com>
2020-08-25 10:56:44 -0500

    testing Correlator::FindMaximum

--- main.cpp
+++ main.cpp
@@ -40,7 +40,8 @@ int main (int argc, char *argv[])
 
 	Correlator testCorrelator("MV", 0.001);
 
-	cout << "Testing MVModel: " << testCorrelator.F(0.5, 0.1, 0.0023, 1) << endl;
+	cout << "Testing MVModel: " << testCorrelator.F(0.5, 0.1, 0.0023, 1) << endl
+	<<	"FindMaximum: " << testCorrelator.FindMaximum(pow(10, -5), 0.0023, 1, 0, 1, 0.001) << endl;
 /*
 	//  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	ofstream quark_output;
