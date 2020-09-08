1260c88ec3b8d34f2deef1bf4b8ab554746d86f9
pcarzon <pcarzon@users.noreply.github.com>
2020-09-08 13:54:52 -0500

    test

--- main.cpp
+++ main.cpp
@@ -66,7 +66,7 @@ int main (int argc, char *argv[])
 
 				if (!testEvent.UpdateDensity(testQuarks)) { exit(0); }
 
-				if (testQuarks.GetCharge()[0] > 0)	{ cout << "sampled quark charge " << testQuarks.GetCharge()[0] << endl; break; }
+//				if (testQuarks.GetCharge()[0] > 0)	{ cout << "sampled quark charge " << testQuarks.GetCharge()[0] << endl; break; }
 				eventcount++;
 			}
 
