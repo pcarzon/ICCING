5afa4afdc0c52090a13481974c0d76e316671bfc
pcarzon <pcarzon@users.noreply.github.com>
2020-11-17 14:25:34 -0600

    test

--- NewAnalysis/io.cpp
+++ NewAnalysis/io.cpp
@@ -58,7 +58,7 @@ IO::IO(string configFile)
       //#CONFIGPARAM
     }// End of switch
   }// End of while loop
-
+cout << output_folder << endl;
   input.close();
 //  OutputConfig(output_dir + "run_parameters" + to_string(current_event) + ".dat");
 }// End of Class constructor
