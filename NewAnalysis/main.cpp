06bf5dcd0ed83c720411c17d30ce78eea004c7ac
pcarzon <pcarzon@users.noreply.github.com>
2020-11-18 10:38:25 -0600

    test

--- NewAnalysis/main.cpp
+++ NewAnalysis/main.cpp
@@ -19,7 +19,20 @@ int main (int argc, char *argv[])
 
 	vector<Event> events = inOut.ReadEvents();
 
-	Sorter sort_events(events);
+	vector<int> centrality_cuts = {1, 5, 10};
+
+	for (int cent = 0; cent < centrality_cuts.size(); cent++)
+	{
+		Sorter sort_events(events);
+		vector<vector<Event>> events_centbins = sort_events.SortEccentricitiesIntoCentralityBins(centrality_cuts[cent]);
+
+		Calculator calculate_centbins(events_centbins);
+		inOut.OutputObservables(calculate_centbins.CalculateCummulants(), "/" + centrality_cuts[cent] + "per_centbins/Cummulants_4and6part.dat");
+		inOut.OutputObservables(calculate_centbins.Calculate_2Particle_Cummulants(), "/" + centrality_cuts[cent] + "per_centbins/Cummulants_2part.dat");
+		inOut.OutputObservables(calculate_centbins.Calculate_4and6Particle_Cummulants(), "/" + centrality_cuts[cent] + "per_centbins/Cummulants_4and6part_noRatio.dat");
+		inOut.OutputObservables(calculate_centbins.Calculate_V2_V3_CummulantRatio(), "/" + centrality_cuts[cent] + "per_centbins/Cummulants_E2_E3_Ratio.dat");
+	}
+/*	Sorter sort_events(events);
 	vector<vector<Event>> events_1per_centbins = sort_events.SortEccentricitiesIntoCentralityBins(1);
 	vector<vector<Event>> events_5per_centbins = sort_events.SortEccentricitiesIntoCentralityBins(5);
 	vector<vector<Event>> events_10per_centbins = sort_events.SortEccentricitiesIntoCentralityBins(10);
@@ -44,6 +57,6 @@ int main (int argc, char *argv[])
 	inOut.OutputObservables(calculate_10per_centbins.Calculate_4and6Particle_Cummulants(), "/10per_centbins/Cummulants_4and6part_noRatio.dat");
 	inOut.OutputObservables(calculate_10per_centbins.Calculate_V2_V3_CummulantRatio(), "/10per_centbins/Cummulants_E2_E3_Ratio.dat");
 //	inOut.OutputObservables(calculate_10per_centbins.CalculatePtObservables(), "/10per_centbins/Cummulants_PtObservables.dat");
-
+*/
 	return 0;
 }
