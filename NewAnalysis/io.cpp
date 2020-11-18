fa527dfed9be01ba1fa8bfd790abc5507bab82cb
pcarzon <pcarzon@users.noreply.github.com>
2020-11-18 10:05:36 -0600

    test

--- NewAnalysis/io.cpp
+++ NewAnalysis/io.cpp
@@ -31,7 +31,7 @@ IO::IO(string configFile)
       //  input >> input_var (Read in value of var_type)
       //  break; (Stop checking switch and move on)
       case eventinputfile: input >> event_input_file;
-      case outputfolder: input >> output_folder;
+//      case outputfolder: input >> output_folder;
 
       case dataformat:
         int num = 0;
