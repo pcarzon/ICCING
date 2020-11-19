da31a7b4ff955e11045204638b637ff358ca1517
pcarzon <pcarzon@users.noreply.github.com>
2020-11-19 14:29:29 -0600

    test

--- NewAnalysis/io.cpp
+++ NewAnalysis/io.cpp
@@ -198,6 +198,7 @@ void IO::OutputObservables(vector<vector<double>> observables, string file)
   ofstream output;
   output.open(output_folder + file);
 
+  cout << output_folder + file << " " << observables[0].size() << endl;
   for (int i = 0; i < observables.size(); i++)
   {
     for (int j = 0; j < observables[i].size(); j++)
